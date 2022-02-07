f9d231cd08628eb260f38037ccc1012409100e69
Biswarup35 <biswarup35@gmail.com>
2022-02-07 12:58:10 +0530

    adding 404 page

---
+++ src/views/NotFound.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "../components";
+
+const NotFound = () => (
+  <Stack
+    style={{ minHeight: `90vh` }}
+    direction="column"
+    justifyContent="center"
+    alignItems="center"
+  >
+    <h1>404 - Page Not Found!</h1>
+  </Stack>
+);
+
+export default NotFound;
